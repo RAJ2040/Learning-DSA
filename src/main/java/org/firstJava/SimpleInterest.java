959ba05e5416c2a7bc92b412dca5fa2627322038
Muthuraj Nadar <rajnadar2030@gmail.com>
2024-12-20 20:12:31 +0530

    made changes in SimpleInterest and Sum

--- src/main/java/org/firstJava/SimpleInterest.java
+++ src/main/java/org/firstJava/SimpleInterest.java
@@ -1,4 +1,5 @@
 package org.firstJava;
 
 public class SimpleInterest {
+
 }
--- src/main/java/org/flowOfProgram/Sum.java
+++ src/main/java/org/flowOfProgram/Sum.java
@@ -11,9 +11,8 @@ public class Sum {
         System.out.print("Enter 2nd Number : ");
         int num2 = in.nextInt();
 
-//        int sum = num1+num2;
-//        System.out.println(sum);
-        System.out.println(num1+num2);
+        int sum = num1+num2;
+        System.out.println("Sum of given number is : "+sum);
 
     }
 }
